e79c1f37516752659fa829f9f1ef0aed28bf6103
andmalt <andrea.maltempi@gmail.com>
2021-09-08 16:21:11 +0200

    first commit

---
+++ Js/myscript.js
@@ -0,0 +1,14 @@
+
+let card = {
+    name:'Illuna, Apex of Wishes',
+    launchCost : ['RedEarth','BlueEarth','GreenEarth','ColorlessEarth','ColorlessEarth'],
+    combinedManaCost = launchCost.length,
+    cardType: 'legendary creature',
+    subType: 'elemental',
+    expansion: {
+        expansionID: 7,
+        name: 'Ikoria: Lair of Behemoths',
+        rarity: 'mythic rare',
+    }
+}
+
