17197a0f29ea0a44cc0de85cd0a1eaf8649efb07
andmalt <andrea.maltempi@gmail.com>
2021-09-08 22:09:51 +0200

    inserted abilities and image

--- Js/myscript.js
+++ Js/myscript.js
@@ -1,12 +1,12 @@
 
 let card = {
     name:'Adarkar Valkyrie',
-    launchCost : ['whiteEarth','whiteEarth','ColorlessEarth','ColorlessEarth','ColorlessEarth','ColorlessEarth'],
+    launchCost : ['white','white','Colorless','Colorless','Colorless','Colorless'],
     combinedManaCost : function(){
         let cost = this.launchCost.length;
         return cost;
     },
-    cardType: 'creature',
+    type: 'creature',
     subType: 'angel',
     expansion: {
         expansionID: 7,
@@ -16,15 +16,19 @@ let card = {
     },
 
     cardNumber: 60,
-    
+
     flavourText:{
         author:'Jeremy Jarvis',
         quote:'She does not escort the dead to the afterlife, but resurrect them to make them fight and die again.'
     },
     abilities:[{
-        name:'flying'
+        name:'flying',
+        cost:'not any',
+        description:'creature can fly'
     },{
-        name:'vigilance'
+        name:'vigilance',
+        cost:'not any',
+        description:'creature not tapping if attack'
     },{
         name:'return from the dead',
         cost:['tapping'],
@@ -43,5 +47,41 @@ let card = {
     cardImage:'img/adarkar-valkyrie.jpg',
 };
 
-console.log(card.combinedManaCost());
-console.log(card.launchCost.length);
+let abilityContent='';
+
+if(card.abilities.length){
+    abilityContent = '<ul class="card-ability">';
+    for(let i = 0; i < card.abilities.length; i++){
+        let currentAbility = card.abilities[i];
+        abilityContent += '---------------------------';
+        abilityContent +=   `<li>Name: ${currentAbility.name}</li>`;
+        abilityContent +=   `<li>cost: ${currentAbility.cost} </li>
+                            <li>description: ${currentAbility.description} </li>`;              
+        abilityContent += '---------------------------';               
+    }
+    abilityContent += '</ul>';
+}
+
+let cardDisplay = 
+`<ul class="card-element">
+    <li>Name: ${card.name}</li>
+    <li>Launch combined cost mana: ${card.launchCost.join(' - ')}</li>
+    <li>Total launch cost: ${card.combinedManaCost()} mana</li>
+    <li>Card Type: ${card.type} - ${card.subType}</li>
+    <li>Expansion: 
+        <ul class="expansion">
+            <li>Name: ${card.expansion.name}</li>
+            <li>Rarity: ${card.expansion.rarity}</li>
+            <li>Tot. cards: ${card.expansion.numberCards}</li>
+            <li>Card number: ${card.cardNumber}</li>
+        </ul>
+    </li>
+    <li>Ability:${abilityContent}</li>
+    <li>Flavour text:<em> ${card.flavourText.quote}</em></li>
+    <li>Illustrator: ${card.illustrator.name} - ${card.illustrator.surname} </em></li>
+    <li>Costitution: ${card.constitution}</li>
+    <li>Strength: ${card.strength}</li>
+    <img src="img/adarkar-valkyrie.jpg" alt="card image">
+</ul>`
+
+document.getElementById('card-deck').innerHTML += cardDisplay;
